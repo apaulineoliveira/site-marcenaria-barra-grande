920d8b562f58b322328143167639da556895da25
apaulineoliveira <apaulineoliveira@gmail.com>
2022-11-17 21:02:11 -0300

    update

--- src/script.js
+++ src/script.js
@@ -5,3 +5,26 @@ function Enviar() {
         alert('Obrigado, '  + nome.value +  ' seu projeto foi encaminhado para a nossa equipe com sucesso!');     
     }
 }
+
+
+const filterContainer = document.querySelector(".gallery-filter"),
+galleryItems = document.querySelectorAll(".gallery-item");
+
+filterContainer.addEventListener("click", (event) =>{
+    if(event.target.classList.contains("filter-item")){
+        filterContainer.querySelector(".active").classList.remove("active");
+
+        event.target.classList.add("active");
+        const filterValue = event.target.getAttribute("data-filter");
+        galleryItems.forEach((item) =>{
+            if(item.classList.contains(filterValue) || filterValue === 'all'){
+                item.classList.remove("hide");
+                item.classList.add("show");
+            }
+            else {
+                item.classList.remove("show");
+                item.classList.add("hide");
+            }
+        });
+    }
+});
